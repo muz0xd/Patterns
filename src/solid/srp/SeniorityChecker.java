27815ee560640e0833254fcb538091a710140ba9
redo <mail@mail.com>
2023-05-02 11:10:22 +0300

    add single responsible classes

---
+++ src/solid/srp/SeniorityChecker.java
@@ -0,0 +1,9 @@
+package solid.srp;
+
+public class SeniorityChecker {
+
+    public String checkSeniority(double experienceInYears) {
+        return experienceInYears > 5 ? "senior" : "junior";
+    }
+    
+}
