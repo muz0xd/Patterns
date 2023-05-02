fada200169f9e95baa39e4b2d4b5129f36222e44
redo <mail@mail.com>
2023-05-02 10:45:27 +0300

    delete empId from constructor

--- src/solid/srp_without/Employee.java
+++ src/solid/srp_without/Employee.java
@@ -5,15 +5,15 @@ import java.util.Random;
 public class Employee {
     public String firstName, lastName, empId;
     public double experienceInYears;
-    public Employee(String firstName, String lastName, String empId, double experienceInYears) {
+    public Employee(String firstName, String lastName, double experienceInYears) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.experienceInYears = experienceInYears;
     }
     
     public void displayEmpDetail() {
-        System.out.println("The employee name:" + lastName + "," + firstName);
-        System.out.println("This employee has" + experienceInYears + "years of expirience.");
+        System.out.println("The employee name: " + lastName + ", " + firstName);
+        System.out.println("This employee has " + experienceInYears + " years of expirience.");
     }
     
     public String checkSeniority (double experienceInYears) {
